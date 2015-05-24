00b36de7b9ab64642a8ac5d96137caec121608bd
Lance <computerwizz98@yahoo.com>
2015-05-23 22:27:04 -0700

    Made minor edits to gamemanager.cpp

--- src/tetrisGameManager.cpp
+++ src/tetrisGameManager.cpp
@@ -8,14 +8,26 @@ void tetrisGameManager::drawLogic(sf::RenderWindow& window)
 {
     sf::RectangleShape block(sf::Vector2f(19, 19));
     block.setOutlineThickness(5);
-    blockGrid[0][0] = Cyan;
-    blockGrid[2][7] = Cyan;
-    blockGrid[6][21] = Cyan;
-    blockGrid[9][18] = Cyan;
-    blockGrid[3][5] = Cyan;
     for(int x = 0; x < 9; x++)
     {
         for(int y = 0; y < 23; y++)
+        {
+            blockGrid[x][y] = Empty;
+        }
+    }
+    blockGrid[0][0] = Cyan;
+    blockGrid[1][0] = Cyan;
+    blockGrid[2][0] = Cyan;
+    blockGrid[3][0] = Cyan;
+    blockGrid[4][0] = Cyan;
+    blockGrid[5][0] = Cyan;
+    blockGrid[6][0] = Cyan;
+    blockGrid[7][0] = Cyan;
+    blockGrid[8][0] = Cyan;
+    blockGrid[9][0] = Cyan;
+    for(int x = 0; x < 10; x++)
+    {
+        for(int y = 0; y < 24; y++)
         {
             block.setPosition(x * 29 + 5, y * 29 + 5);
             switch(blockGrid[x][y])
@@ -26,6 +38,7 @@ void tetrisGameManager::drawLogic(sf::RenderWindow& window)
                 break;
             case Cyan:
                 block.setFillColor(sf::Color::Cyan);
+                block.setOutlineColor(sf::Color::Red);
                 break;
             case Yellow:
                 block.setFillColor(sf::Color::Yellow);
